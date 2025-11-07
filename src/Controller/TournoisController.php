2381a292ca383cbcf94c230f4d5cd94a4a6b8b2e
A.Wagner <aniswagner6@gmail.com>
2025-11-07 09:41:36 +0100

    Agora 4.2 finie

--- src/Controller/TournoisController.php
+++ src/Controller/TournoisController.php
@@ -14,8 +14,26 @@ final class TournoisController extends AbstractController
     {
         // Vérification de l'authentification (utilise la session PHP native ou Symfony)
         if (isset($_SESSION['idUtilisateur']) || $session->has('idUtilisateur')) {
+            // Préparation des données de session pour le template
+            $sessionData = [];
+            if ($session && $session->has('idUtilisateur')) {
+                $sessionData = [
+                    'idUtilisateur' => $session->get('idUtilisateur'),
+                    'nomUtilisateur' => $session->get('nomUtilisateur'),
+                    'prenomUtilisateur' => $session->get('prenomUtilisateur'),
+                ];
+            } elseif (isset($_SESSION['idUtilisateur'])) {
+                $sessionData = [
+                    'idUtilisateur' => $_SESSION['idUtilisateur'],
+                    'nomUtilisateur' => $_SESSION['nomUtilisateur'] ?? '',
+                    'prenomUtilisateur' => $_SESSION['prenomUtilisateur'] ?? '',
+                ];
+            }
+            
             return $this->render('tournois/index.html.twig', [
                 'controller_name' => 'TournoisController',
+                'menuActif' => 'Jeux',
+                'session' => $sessionData
             ]);
         } else {
             return $this->render('connexion.html.twig');
